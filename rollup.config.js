6faed4997422511a93486e67b2042c799471bcd0
Kushuh <kuzanisu@gmail.com>
2020-05-12 02:08:12 +0200

    removed useless import and converted to es6 export

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import {terser} from 'rollup-plugin-terser';
