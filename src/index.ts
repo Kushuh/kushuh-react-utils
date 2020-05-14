42b9e8367cf8805184edfe17ed58c131e83dc6dd
Kushuh <kuzanisu@gmail.com>
2020-05-14 06:42:45 +0200

    fixed publish options

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {addPropsToChildren} from './react-component/addPropsToChildren';
 import {setStateAsync} from './react-component/setStateAsync';
 import {loadFonts, getFontInformation} from './dom/style/fonts';
 
-module.exports = {
+export default {
 	addPropsToChildren,
 	setStateAsync,
 	loadFonts,
