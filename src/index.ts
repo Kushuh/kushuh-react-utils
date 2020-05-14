0d2e5924eb0723b5a6d231b9b45a2a8d312a6ec4
Kushuh <kuzanisu@gmail.com>
2020-05-14 06:39:08 +0200

    exported as cjs module

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {addPropsToChildren} from './react-component/addPropsToChildren';
 import {setStateAsync} from './react-component/setStateAsync';
 import {loadFonts, getFontInformation} from './dom/style/fonts';
 
-export {
+module.exports = {
 	addPropsToChildren,
 	setStateAsync,
 	loadFonts,
